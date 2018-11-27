583646a3079b748cfcfe4f1faf077ba868d36696
KimReady <kjb4380@gmail.com>
2018-11-27 20:19:30 +0900

    first commit

---
+++ inc/smart-security-entrance.h
@@ -0,0 +1,12 @@
+#ifndef __smart-security-entrance_H__
+#define __smart-security-entrance_H__
+
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "smart-security-entrance"
+
+
+#endif /* __smart-security-entrance_H__ */
