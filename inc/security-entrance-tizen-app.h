116428d79163cde98e6aa2304b6e03aeda90e895
wh2per <93negaro@gmail.com>
2018-11-28 02:36:04 +0900

    Implement to save the Image by Camera Sensor

--- inc/security-entrance-tizen-app.h
+++ inc/security-entrance-tizen-app.h
@@ -6,7 +6,15 @@
 #ifdef  LOG_TAG
 #undef  LOG_TAG
 #endif
-#define LOG_TAG "smart-security-entrance"
+#define LOG_TAG "security-entrance-tizen-app"
 
+#define MV_RESULT_COUNT_MAX 30
+#define MV_RESULT_LENGTH_MAX (MV_RESULT_COUNT_MAX * 4) //4(x, y, w, h) * COUNT
+#define IMAGE_INFO_MAX ((8 * MV_RESULT_LENGTH_MAX) + 4)
+
+#define IMAGE_WIDTH 320
+#define IMAGE_HEIGHT 240
+#define CAMERA_IMAGE_QUALITY 100 //1~100
+#define CAMERA_PREVIEW_INTERVAL_MIN 50
 
 #endif /* __smart-security-entrance_H__ */
--- src/security-entrance-tizen-app.c
+++ src/security-entrance-tizen-app.c
@@ -4,15 +4,22 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <Ecore.h>
+#include <glib.h>
+#include <camera.h>
+#include <mv_common.h>
+#include <pthread.h>
 #include <security-entrance-tizen-app.h>
 
 #include "log.h"
 #include "resource_infrared_motion_sensor.h"
 #include "resource_led.h"
 #include "resource_servo_motor.h"
+#include "resource_camera.h"
+#include "controller_image.h"
+
 
 // Duration for a timer
-#define TIMER_GATHER_INTERVAL (1.0f)
+#define TIMER_GATHER_INTERVAL (2.0f)
 
 // Motion sensor info
 #define SENSOR_MOTION_GPIO_NUMBER (130)
@@ -26,13 +33,26 @@
 #define SERVO_MOTOR_DUTY_CYCLE_COUNTER_CLOCKWISE 1.0
 #define SERVO_MOTOR_DUTY_CYCLE_CLOCKWISE 2.0
 
-#define SENSOR_GATHER_INTERVAL (1.0f)
+#define SENSOR_GATHER_INTERVAL (2.0f)
 
 
 typedef struct app_data_s {
 	Ecore_Timer *getter_timer_PL;		// PIR과 LED용 app_data : 개더링 타임
 	Ecore_Timer *getter_timer_MT;		// 모터용 app_data : 개더링 타임
 	int door;						// 모터용 app_data : 보안카드가 있으면 1, 없으면 0
+
+	// 카메라용 app_data
+	unsigned int latest_image_width;
+	unsigned int latest_image_height;
+	char *latest_image_info;
+	int latest_image_type; // 0: image during camera repositioning, 1: single valid image but not completed, 2: fully validated image
+	unsigned char *latest_image_buffer;
+
+	Ecore_Thread *image_writter_thread;
+	pthread_mutex_t mutex;
+
+	char* temp_image_filename;
+	char* latest_image_filename;
 } app_data;
 
 static app_data *g_ad = NULL;
@@ -109,7 +129,7 @@ static Eina_Bool __start_servo_motor(void *data){
 	    return ECORE_CALLBACK_CANCEL;
 	}
 
-	_E("door : %d", ad->door);
+	_I("door : %d", ad->door);
 	if(ad->door==1){
 		on = 1;		// 열기
 	}else {
@@ -162,10 +182,213 @@ static void _start_gathering(void *data)
 }
 
 
+static mv_colorspace_e __convert_colorspace_from_cam_to_mv(camera_pixel_format_e format)
+{
+	mv_colorspace_e colorspace = MEDIA_VISION_COLORSPACE_INVALID;
+	switch (format) {
+	case CAMERA_PIXEL_FORMAT_NV12:
+		colorspace = MEDIA_VISION_COLORSPACE_NV12;
+		break;
+	case CAMERA_PIXEL_FORMAT_NV21:
+		colorspace = MEDIA_VISION_COLORSPACE_NV21;
+		break;
+	case CAMERA_PIXEL_FORMAT_YUYV:
+		colorspace = MEDIA_VISION_COLORSPACE_YUYV;
+		break;
+	case CAMERA_PIXEL_FORMAT_UYVY:
+		colorspace = MEDIA_VISION_COLORSPACE_UYVY;
+		break;
+	case CAMERA_PIXEL_FORMAT_422P:
+		colorspace = MEDIA_VISION_COLORSPACE_422P;
+		break;
+	case CAMERA_PIXEL_FORMAT_I420:
+		colorspace = MEDIA_VISION_COLORSPACE_I420;
+		break;
+	case CAMERA_PIXEL_FORMAT_YV12:
+		colorspace = MEDIA_VISION_COLORSPACE_YV12;
+		break;
+	case CAMERA_PIXEL_FORMAT_RGB565:
+		colorspace = MEDIA_VISION_COLORSPACE_RGB565;
+		break;
+	case CAMERA_PIXEL_FORMAT_RGB888:
+		colorspace = MEDIA_VISION_COLORSPACE_RGB888;
+		break;
+	case CAMERA_PIXEL_FORMAT_RGBA:
+		colorspace = MEDIA_VISION_COLORSPACE_RGBA;
+		break;
+	case CAMERA_PIXEL_FORMAT_NV12T:
+	case CAMERA_PIXEL_FORMAT_NV16:
+	case CAMERA_PIXEL_FORMAT_ARGB:
+	case CAMERA_PIXEL_FORMAT_JPEG:
+	case CAMERA_PIXEL_FORMAT_H264:
+	case CAMERA_PIXEL_FORMAT_INVALID:
+	default :
+		colorspace = MEDIA_VISION_COLORSPACE_INVALID;
+		_E("unsupported format : %d", format);
+		break;
+	}
+
+	return colorspace;
+}
+
+static void __thread_write_image_file(void *data, Ecore_Thread *th)
+{
+	app_data *ad = (app_data *)data;
+	unsigned int width = 0;
+	unsigned int height = 0;
+	unsigned char *buffer = 0;
+	char *image_info = NULL;
+	int ret = 0;
+
+	pthread_mutex_lock(&ad->mutex);
+	width = ad->latest_image_width;
+	height = ad->latest_image_height;
+	buffer = ad->latest_image_buffer;
+	ad->latest_image_buffer = NULL;
+	if (ad->latest_image_info) {
+		image_info = ad->latest_image_info;
+		ad->latest_image_info = NULL;
+	} else {
+		image_info = strdup("00");
+	}
+	pthread_mutex_unlock(&ad->mutex);
+
+	ret = controller_image_save_image_file(ad->temp_image_filename, width, height, buffer, image_info, strlen(image_info));
+	if (ret) {
+		_E("failed to save image file");
+	} else {
+		ret = rename(ad->temp_image_filename, ad->latest_image_filename);
+		if (ret != 0 )
+			_E("Rename fail");
+	}
+	free(image_info);
+	free(buffer);
+}
+
+static void __thread_end_cb(void *data, Ecore_Thread *th)
+{
+	app_data *ad = (app_data *)data;
+
+	pthread_mutex_lock(&ad->mutex);
+	ad->image_writter_thread = NULL;
+	pthread_mutex_unlock(&ad->mutex);
+}
+
+static void __thread_cancel_cb(void *data, Ecore_Thread *th)
+{
+	app_data *ad = (app_data *)data;
+	unsigned char *buffer = NULL;
+
+	_E("Thread %p got cancelled.\n", th);
+	pthread_mutex_lock(&ad->mutex);
+	buffer = ad->latest_image_buffer;
+	ad->latest_image_buffer = NULL;
+	ad->image_writter_thread = NULL;
+	pthread_mutex_unlock(&ad->mutex);
+
+	free(buffer);
+}
+
+static void __copy_image_buffer(image_buffer_data_s *image_buffer, app_data *ad)
+{
+	unsigned char *buffer = NULL;
+
+	pthread_mutex_lock(&ad->mutex);
+	ad->latest_image_height = image_buffer->image_height;
+	ad->latest_image_width = image_buffer->image_width;
+
+	buffer = ad->latest_image_buffer;
+	ad->latest_image_buffer = image_buffer->buffer;
+	pthread_mutex_unlock(&ad->mutex);
+
+	free(buffer);
+}
+
+static void __preview_image_buffer_created_cb(void *data)
+{
+	image_buffer_data_s *image_buffer = data;
+	app_data *ad = (app_data *)image_buffer->user_data;
+	mv_colorspace_e image_colorspace = MEDIA_VISION_COLORSPACE_INVALID;
+	char *info = NULL;
+
+	ret_if(!image_buffer);
+	ret_if(!ad);
+
+	image_colorspace = __convert_colorspace_from_cam_to_mv(image_buffer->format);
+	goto_if(image_colorspace == MEDIA_VISION_COLORSPACE_INVALID, FREE_ALL_BUFFER);
+
+	__copy_image_buffer(image_buffer, ad);
+
+	pthread_mutex_lock(&ad->mutex);
+	info = ad->latest_image_info;
+	ad->latest_image_info = NULL;
+	pthread_mutex_unlock(&ad->mutex);
+	free(info);
+
+	free(image_buffer);
+
+	pthread_mutex_lock(&ad->mutex);
+	if (!ad->image_writter_thread) {
+		ad->image_writter_thread = ecore_thread_run(__thread_write_image_file, __thread_end_cb, __thread_cancel_cb, ad);
+	} else {
+		_E("Thread is running NOW");
+	}
+	pthread_mutex_unlock(&ad->mutex);
+
+	return;
+
+FREE_ALL_BUFFER:
+	free(image_buffer->buffer);
+	free(image_buffer);
+}
+
+
+
 static bool service_app_create(void *user_data)
 {
 
+	app_data *ad = (app_data *)user_data;
+
+	char* shared_data_path = app_get_shared_data_path();
+	if (shared_data_path == NULL) {
+		_E("Failed to get shared data path");
+		goto ERROR;
+	}
+	ad->temp_image_filename = g_strconcat(shared_data_path, "tmp.jpg", NULL);
+	ad->latest_image_filename = g_strconcat(shared_data_path, "latest.jpg", NULL);
+	free(shared_data_path);
+
+		_D("%s", ad->temp_image_filename);
+		_D("%s", ad->latest_image_filename);
+
+	controller_image_initialize();
+
+	pthread_mutex_init(&ad->mutex, NULL);
+
+//	if (__device_interfaces_init(ad))
+//		goto ERROR;
+
+	if (resource_camera_init(__preview_image_buffer_created_cb, ad) == -1) {
+		_E("Failed to init camera");
+		goto ERROR;
+	}
+
+	if (resource_camera_start_preview() == -1) {
+		_E("Failed to start camera preview");
+		goto ERROR;
+	}
+
 	return true;
+
+	ERROR:
+	//	__device_interfaces_fini();
+
+	resource_camera_close();
+	controller_image_finalize();
+
+	pthread_mutex_destroy(&ad->mutex);
+
+	return false;
 }
 
 static void service_app_control(app_control_h app_control, void *user_data)
@@ -179,6 +402,14 @@ static void service_app_terminate(void *user_data)
 	app_data *ad = user_data;
 	int ret = 0;
 
+	// Turn off Camera
+	Ecore_Thread *thread_id = NULL;
+	unsigned char *buffer = NULL;
+	char *info = NULL;
+	gchar *temp_image_filename;
+	gchar *latest_image_filename;
+	_D("App Terminated - enter");
+
 	// Turn off LED light with __set_led()
 	ret = resource_write_led(SENSOR_LED_GPIO_NUMBER, 0);
 	if (ret != 0) {
@@ -188,8 +419,39 @@ static void service_app_terminate(void *user_data)
 	// Close Motion and LED resources
 	resource_close_infrared_motion_sensor();
 	resource_close_led();
+
 	// Close Motor
 	resource_close_servo_motor();
+
+	// Close Camera
+	resource_camera_close();
+
+	pthread_mutex_lock(&ad->mutex);
+	thread_id = ad->image_writter_thread;
+	ad->image_writter_thread = NULL;
+	pthread_mutex_unlock(&ad->mutex);
+
+	if (thread_id)
+		ecore_thread_wait(thread_id, 3.0); // wait for 3 second
+
+	controller_image_finalize();
+
+	pthread_mutex_lock(&ad->mutex);
+	buffer = ad->latest_image_buffer;
+	ad->latest_image_buffer = NULL;
+	info  = ad->latest_image_info;
+	ad->latest_image_info = NULL;
+	temp_image_filename = ad->temp_image_filename;
+	ad->temp_image_filename = NULL;
+	latest_image_filename = ad->latest_image_filename;
+	ad->latest_image_filename = NULL;
+	pthread_mutex_unlock(&ad->mutex);
+	free(buffer);
+	free(info);
+	g_free(temp_image_filename);
+	g_free(latest_image_filename);
+
+	pthread_mutex_destroy(&ad->mutex);
 
 	// Free app data
 	free(ad);
@@ -203,7 +465,7 @@ int main(int argc, char *argv[])
 	service_app_lifecycle_callback_s event_callback;
 
 	ad = calloc(1, sizeof(app_data));
-	ad->door = 0;		// 보안카드 없음으로 초기화
+	ad->door = 1;		// 보안카드 없음으로 초기화
 	retv_if(!ad, -1);
 
 	g_ad = ad;
